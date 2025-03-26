623efd7baabfb53906d83599c44f0fced23f5c75
michael <48704702+mhev@users.noreply.github.com>
2025-03-26 17:45:10 -0500

    Add files via upload

---
+++ RR/firebase-config.template.js
@@ -0,0 +1,9 @@
+// TEMPLATE FILE - Rename to firebase-config.js and add your Firebase credentials
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE"
+}; 
