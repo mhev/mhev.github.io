663522999118bc557119c9ce60cc2d1ac42f9d14
michael <48704702+mhev@users.noreply.github.com>
2025-03-27 19:58:11 -0500

    Add files via upload

---
+++ RR/firebase-config.js
@@ -0,0 +1,10 @@
+// This contains public Firebase web credentials - protected by Firebase Security Rules
+export const firebaseConfig = {
+  apiKey: "AIzaSyA3PLF5XP8Z0nPq8ooS0Q8arghRH4L4MA4",
+  authDomain: "resume-radar-aeef3.firebaseapp.com",
+  projectId: "resume-radar-aeef3",
+  storageBucket: "resume-radar-aeef3.firebasestorage.app",
+  messagingSenderId: "547030628861",
+  appId: "1:547030628861:web:adcf2d89554e267b6dc194",
+  measurementId: "G-LNE8F7EGJS"
+};
